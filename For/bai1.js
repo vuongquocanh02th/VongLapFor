01699cb75739d8718fb9ebbdc0b15c053b642a28
vuongquocanh02th <vuongquocanh300402@gmail.com>
2024-10-11 15:27:44 +0700

    add for file

---
+++ For/bai1.js
@@ -0,0 +1,6 @@
+let text="";
+let i;
+for(i=0;i<5;i++){
+    text += "The number is "+i+"<br>";
+}
+document.getElementById("demo").innerHTML = text;
---
+++ For/bai2.js
@@ -0,0 +1,8 @@
+let so=+prompt("Nhap so");
+let tong=0;
+let i=1;
+for(i=1; i<=so; i+=1){
+    tong+=i;
+
+}
+alert(tong);
